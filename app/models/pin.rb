a0e7ea9be667b8e4cdaa91ef1857c31991b27701
Olivier Sabatier <olivier.sabatier@ieseg.fr>
2013-07-07 17:06:28 +0200

    Adding Paper clip and Images
    resolve the image matter with strong parameters :
    
    def pin_params
          params.require(:pin).permit(:description, :image)
        end

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -8,13 +8,9 @@ class Pin < ActiveRecord::Base
 
 
 validates :description, presence: true 
-#validates :user_id, presence: true
-#validates_attachment :image, presence: true,
-                            #content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
-                            #size: { less_than: 5.megabytes }
-
-
- 
-
+validates :user_id, presence: true
+validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }
 
 end
