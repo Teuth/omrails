4a060dbb65d2c9c20c0a174938b52b7a5004e2d8
Olivier Sabatier <olivier.sabatier@ieseg.fr>
2013-07-08 14:49:56 +0200

    Fixing a little bug uril->url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -9,11 +9,11 @@ class Pin < ActiveRecord::Base
 
 validates :description, presence: true 
 validates :user_id, presence: true
-validates_attachment :image, presence: true,
+validates_attachment :image , presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
 
-def image_remote_uril=(url_value)
+def image_remote_url=(url_value)
 	self.image = URI.parse(url_value) unless url_value.blank?
 	super
 end
